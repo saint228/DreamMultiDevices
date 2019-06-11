1966abe138f0c2f25f6cbe12537b401c376f2ffc
saint228 <saint_228@126.com>
2019-06-12 00:06:11 +0800

    调试xlwings方法

--- tools/Excel.py
+++ tools/Excel.py
@@ -3,26 +3,30 @@ import os
 import time
 
 
-def create_log_excel():
-    exclefile = os.getcwd()  + "\log.xlsx"
+def create_log_excel(nowtime,device):
+    create_time=time.strftime("%m%d%H%M", nowtime)
+    exclefile = create_time+ "_"+ device + "_log.xlsx"
     app = xw.App(visible=True, add_book=False)
     if not os.path.exists(exclefile):
         wb = app.books.add()
+        sheet=wb.sheets("Sheet1")
+        sheet.range('A1').value = ["Time","TotalMemory", "AllocatedMemory","UsedMemory","FreeMemory","TotalCPU","AllocatedCPU"]
         wb.save(exclefile)
-    else:
-        wb = app.books.open(exclefile)
+        print(exclefile)
+    return exclefile, sheet,wb
 
-def input_log(*kargs):
-    pass
-
-
-def record_allocated_memory(self,timeout=30):
-    start_time=time.time()
-    while time.time()-start_time<timeout:
-        nowmemory=self.get_allocated_memory()
-        self.write_excel(nowmemory,"allocated_memory",time.time())
-        time.sleep(0.5)
-
+def record_to_excel(sheet,nowtime,*,TotalMemory,AllocatedMemory,UsedMemory,FreeMemory,TotalCPU,AllocatedCPU):
+    sheet.range("A2").value =time.strftime("%Y-%m-%d %H:%M:%S",nowtime)
+    sheet.range("B2").value=TotalMemory
+    sheet.range("C2").value=AllocatedMemory
+    sheet.range("D2").value=UsedMemory
+    sheet.range("E2").value=FreeMemory
+    sheet.range("F2").value=TotalCPU
+    sheet.range("G2").value=AllocatedCPU
 
 if __name__ == "__main__":
-    create_log_excel()
+    nowtime=time.localtime()
+    print(nowtime)
+    filepath,sheet,wb=create_log_excel(nowtime,"62001")
+    record_to_excel(sheet,nowtime,TotalMemory="1024MB",AllocatedMemory="100MB",UsedMemory="500MB",FreeMemory="300MB",TotalCPU="50%",AllocatedCPU="25%")
+    wb.save()
