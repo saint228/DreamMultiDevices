38cdf7766094bfa92c7e53679ec5f31518700c37
wushengtao <wushengtao@thedream.cc>
2019-08-05 11:24:01 +0800

    改名

---
+++ tools/Email.py
@@ -0,0 +1,32 @@
+__author__ = "无声"
+
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# 第三方 SMTP 服务
+mail_host="smtp.XXX.com"  #设置服务器
+mail_user="XXXX"    #用户名
+mail_pass="XXXXXX"   #口令
+
+sender = 'sender@qq.com'
+receivers = ['xx@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+message = MIMEText('自动化测试邮件', 'plain', 'utf-8')
+message['From'] = Header("自动化测试", 'utf-8')
+message['To'] = Header("测试", 'utf-8')
+
+subject = 'Python SMTP 邮件测试'
+message['Subject'] = Header(subject, 'utf-8')
+
+try:
+    smtpObj = smtplib.SMTP()
+    smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+    smtpObj.login(mail_user, mail_pass)
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException:
+    print("Error: 无法发送邮件")
